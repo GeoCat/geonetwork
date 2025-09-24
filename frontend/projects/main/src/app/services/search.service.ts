5d4273a0d973ca26c009ae2e52cb75d886475dea
Francois Prunayre <fx.prunayre@gmail.com>
2025-09-24 10:10:57 +0200

    Frontend / Example to get records title.

--- frontend/projects/main/src/app/services/search.service.ts
+++ frontend/projects/main/src/app/services/search.service.ts
@@ -1,8 +1,9 @@
 import { inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Result } from '../stores/result';
-import { IndexRecord, elasticsearch} from 'gn-api-client';
+import { IndexRecord, elasticsearch } from 'gn-api-client';
 import { SearchService as ApiSearchService } from 'gn4-api-client';
+import { SearchTotalHits } from '../../../../gn-api-client/src/lib/elasticsearch/types';
 
 @Injectable()
 export class SearchService {
@@ -12,21 +13,20 @@ export class SearchService {
   // We can discuss what to put in the state. Can be only an IndexRecord[]
   // and avoid to tight couple the state with ES response (we may use OGC API Records in the future)
   getByQuery(query: string): Observable<Result[]> {
-
     let searchRequest: elasticsearch.SearchRequest = {
       from: 0,
-      size: 10
+      size: 10,
     };
 
-    this.searchService.search(searchRequest).subscribe(
-      (response: elasticsearch.SearchResponse) => {
-        console.log(response.hits);
-      }
-    )
+    this.searchService.search(searchRequest).subscribe((response: elasticsearch.SearchResponse<IndexRecord>) => {
+      let numberOfResults: SearchTotalHits = response.hits.total as elasticsearch.SearchTotalHits;
+      console.log(numberOfResults.value);
+      console.log(response.hits.hits[0]._source?.resourceTitleObject);
+    });
 
     const mockResults: Result[] = [
       { id: '1', title: `Result for ${query} - 1`, description: 'Mock description 1' },
-      { id: '2', title: `Result for ${query} - 2`, description: 'Mock description 2' }
+      { id: '2', title: `Result for ${query} - 2`, description: 'Mock description 2' },
     ];
     return of(mockResults);
   }
