e6e6976ba268974f4378e8861ed193bf9b2fd1ad
robinesavert <robine.savert@geocat.net>
2025-10-13 14:12:39 +0200

    replace dropdown multiselects with checkboxes

--- frontend/projects/main/src/app/components/facet-component/facet-component.ts
+++ frontend/projects/main/src/app/components/facet-component/facet-component.ts
@@ -1,61 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-
-import { MultiSelect } from 'primeng/multiselect';
 import { FormsModule } from '@angular/forms';
 import { FloatLabel } from 'primeng/floatlabel';
 import { TreeSelect } from 'primeng/treeselect';
 import { NodeService } from '../../services/nodeservice.service';
+import { CommonModule } from '@angular/common';
+import { Accordion, AccordionContent, AccordionHeader, AccordionPanel } from 'primeng/accordion';
+import {Checkbox} from 'primeng/checkbox';
 
 interface Catalogue {
   name: string;
-  code: string;
+  key: string;
 }
 
 interface Theme {
   name: string;
-  code: string;
+  key: string;
 }
 
 interface Keyword {
   name: string;
-  code: string;
+  key: string;
 }
 
 interface Year {
   name: string;
-  code: string;
+  key: string;
 }
 
 interface resourceType {
   name: string;
-  code: string;
+  key: string;
 }
 
 interface Availability {
   name: string;
-  code: string;
+  key: string;
 }
+
 
 @Component({
   selector: 'app-facet-component',
   templateUrl: './facet-component.html',
   styleUrl: './facet-component.scss',
   standalone: true,
-  imports: [MultiSelect, FormsModule, FloatLabel, TreeSelect],
+  imports: [FormsModule, FloatLabel, TreeSelect, AccordionHeader, AccordionContent, AccordionPanel, Accordion, CommonModule, Checkbox],
   providers: [NodeService],
 })
+
 export class FacetComponent implements OnInit {
   nodes!: any[];
-
   selectedNodes: any;
-
-  catalogue!: Catalogue[];
+  catalogues!: Catalogue[];
   inspireThemes!: Theme[];
   keywords!: Keyword[];
   years!: Year[];
   resourceTypes!: resourceType[];
   availabilities!: Availability[];
-
   selectedCatalogue!: Catalogue[];
   selectedTheme!: Theme[];
   selectedKeyword!: Keyword[];
@@ -66,69 +66,70 @@ export class FacetComponent implements OnInit {
   constructor(private nodeService: NodeService) {
     this.nodeService.getFiles().then((files) => (this.nodes = files));
 
-    this.catalogue = [
-      { name: 'Catalogue 1', code: '1' },
-      { name: 'Catalogue 2', code: '2' },
-      { name: 'Catalogue 3', code: '3' },
+    this.catalogues = [
+      { name: 'Catalogue 1', key: '1' },
+      { name: 'Catalogue 2', key: '2' },
+      { name: 'Catalogue 3', key: '3' },
     ];
 
     this.inspireThemes = [
-      { name: 'Theme 1', code: '1' },
-      { name: 'Theme 2', code: '2' },
-      { name: 'Theme 3', code: '3' },
-      { name: 'Theme 4', code: '4' },
-      { name: 'Theme 5', code: '5' },
-      { name: 'Theme 6', code: '6' },
+      { name: 'Theme 1', key: '1' },
+      { name: 'Theme 2', key: '2' },
+      { name: 'Theme 3', key: '3' },
+      { name: 'Theme 4', key: '4' },
+      { name: 'Theme 5', key: '5' },
+      { name: 'Theme 6', key: '6' },
     ];
 
     this.keywords = [
-      { name: 'Marine', code: 'marine' },
-      { name: 'Ocean', code: 'ocean' },
-      { name: 'Sea', code: 'sea' },
-      { name: 'Rivers', code: 'rivers' },
-      { name: 'Lakes', code: 'lakes' },
-      { name: 'Coast', code: 'coast' },
-      { name: 'Water', code: 'water' },
-      { name: 'Wildlife', code: 'wildlife' },
-      { name: 'Biodiversity', code: 'biodiversity' },
-      { name: 'Ecosystem', code: 'ecosystem' },
-      { name: 'Environment', code: 'environment' },
-      { name: 'Conservation', code: 'conservation' },
-      { name: 'Sustainability', code: 'sustainability' },
-      { name: 'Biodiversity', code: 'biodiversity' },
+      { name: 'Marine', key: 'marine' },
+      { name: 'Ocean', key: 'ocean' },
+      { name: 'Sea', key: 'sea' },
+      { name: 'Rivers', key: 'rivers' },
+      { name: 'Lakes', key: 'lakes' },
+      { name: 'Coast', key: 'coast' },
+      { name: 'Water', key: 'water' },
+      { name: 'Wildlife', key: 'wildlife' },
+      { name: 'Biodiversity', key: 'biodiversity' },
+      { name: 'Ecosystem', key: 'ecosystem' },
+      { name: 'Environment', key: 'environment' },
+      { name: 'Conservation', key: 'conservation' },
+      { name: 'Sustainability', key: 'sustainability' },
+      { name: 'Biodiversity', key: 'biodiversity' },
     ];
 
     this.years = [
-      { name: '2025', code: '2025' },
-      { name: '2024', code: '2024' },
-      { name: '2023', code: '2023' },
-      { name: '2022', code: '2022' },
-      { name: '2021', code: '2021' },
-      { name: '2020', code: '2020' },
-      { name: '2019', code: '2019' },
-      { name: '2018', code: '2018' },
-      { name: '2017', code: '2017' },
+      { name: '2025', key: '2025' },
+      { name: '2024', key: '2024' },
+      { name: '2023', key: '2023' },
+      { name: '2022', key: '2022' },
+      { name: '2021', key: '2021' },
+      { name: '2020', key: '2020' },
+      { name: '2019', key: '2019' },
+      { name: '2018', key: '2018' },
+      { name: '2017', key: '2017' },
     ];
 
     this.resourceTypes = [
-      { name: 'Data Set', code: 'dataset' },
-      { name: 'Collection', code: 'collection' },
-      { name: 'API', code: 'api' },
+      { name: 'Data Set', key: 'dataset' },
+      { name: 'Collection', key: 'collection' },
+      { name: 'API', key: 'api' },
     ];
 
     this.availabilities = [
-      { name: 'Documents', code: 'documents' },
-      { name: 'Data Set', code: 'dataset' },
-      { name: 'API', code: 'api' },
-      { name: 'Service', code: 'service' },
-      { name: 'Web Map', code: 'webmap' },
-      { name: 'Web Scene', code: 'webscene' },
-      { name: 'Web Mapping Application', code: 'webmappingapplication' },
-      { name: 'Mobile Application', code: 'mobileapplication' },
-      { name: 'Desktop Application', code: 'desktopapplication' },
-      { name: 'Image', code: 'image' },
-      { name: 'XML', code: 'collection' },
+      { name: 'Documents', key: 'documents' },
+      { name: 'Data Set', key: 'dataset' },
+      { name: 'API', key: 'api' },
+      { name: 'Service', key: 'service' },
+      { name: 'Web Map', key: 'webmap' },
+      { name: 'Web Scene', key: 'webscene' },
+      { name: 'Web Mapping Application', key: 'webmappingapplication' },
+      { name: 'Mobile Application', key: 'mobileapplication' },
+      { name: 'Desktop Application', key: 'desktopapplication' },
+      { name: 'Image', key: 'image' },
+      { name: 'XML', key: 'collection' },
     ];
+
   }
 
   ngOnInit(): void {}
