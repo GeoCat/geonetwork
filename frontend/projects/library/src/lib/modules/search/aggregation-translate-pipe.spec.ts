ee55204e38bc41879c3f7593fe006b7af02f8d34
Francois Prunayre <fx.prunayre@gmail.com>
2025-10-16 09:26:47 +0200

    Frontend / Search / Add translator pipe / Test.

--- frontend/projects/library/src/lib/modules/search/aggregation-translate-pipe.spec.ts
+++ frontend/projects/library/src/lib/modules/search/aggregation-translate-pipe.spec.ts
@@ -1,8 +1,38 @@
 import { AggregationTranslatePipe } from './aggregation-translate-pipe';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TestBed } from '@angular/core/testing';
+import { MockTranslateService } from '../../shared/translate.service.mock';
 
 describe('AggregationTranslatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new AggregationTranslatePipe();
-    expect(pipe).toBeTruthy();
+  let pipe: AggregationTranslatePipe;
+  let mockTranslateService: MockTranslateService;
+
+  beforeEach(async () => {
+    mockTranslateService = new MockTranslateService();
+
+    await TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
+      providers: [
+        { provide: TranslateService, useValue: mockTranslateService },
+        AggregationTranslatePipe,
+      ],
+    });
+    pipe = TestBed.inject(AggregationTranslatePipe);
+  });
+
+  it('returns the translation for a known aggregation and value', () => {
+    expect(pipe.transform('1', 'group')).toBe('Odatis');
+  });
+
+  it('returns the translation for a know value and unknown aggregation', () => {
+    expect(pipe.transform('dataset', 'xyz')).toBe('Dataset');
+  });
+
+  it('returns the value if translation is missing', () => {
+    expect(pipe.transform('adz', 'xyz')).toBe('adz');
+  });
+
+  it('returns the number as is when value is a number', () => {
+    expect(pipe.transform(42, 'isTemplate')).toBe(42);
   });
 });
